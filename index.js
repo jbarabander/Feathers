4486895aa880bc0abd4923b138a2ec216632405b
Justin Barabander <jbarabander@gmail.com>
2015-11-23 22:03:36 -0500

    Added ES5 compatible version

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-'use strict'
+'use strict';
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
+
 var utils = require('./utilities');
 
-module.exports = function(P) {
-	P.remember = function() {
+module.exports = function (P) {
+	P.remember = function () {
 		var arr = [];
 		var accumFunc;
 		var args = Array.prototype.slice.call(arguments);
 		var lastArg = args[args.length - 1];
 		var lastArgType = typeof args[args.length - 1];
-		if(Array.isArray(lastArg)) {
-			accumFunc = P.resolve(args[0](...lastArg));
+		if (Array.isArray(lastArg)) {
+			accumFunc = P.resolve(args[0].apply(args, _toConsumableArray(lastArg)));
 			args.splice(args.length - 1, 1);
-		}
-		else if(lastArgType === 'function') accumFunc = P.resolve(args[0]());
-		else throw new TypeError(`Last argument expected to be a function or array.  Got instead - ${lastArgType}`);
+		} else if (lastArgType === 'function') accumFunc = P.resolve(args[0]());else throw new TypeError('Last argument expected to be a function or array.  Got instead - ' + lastArgType);
 
-		accumFunc = accumFunc.then(value => utils.valueAccum(value, arr));
-		for(let i = 1; i < args.length; i++) {
-			accumFunc = accumFunc.then(args[i]).then(value => utils.valueAccum(value, arr));
+		accumFunc = accumFunc.then(function (value) {
+			return utils.valueAccum(value, arr);
+		});
+		for (var i = 1; i < args.length; i++) {
+			accumFunc = accumFunc.then(args[i]).then(function (value) {
+				return utils.valueAccum(value, arr);
+			});
 		}
 		return accumFunc.then(args[args.length]);
 	};
@@ -35,43 +40,36 @@ module.exports = function(P) {
 	// };
 
 	// P.order = function() {
-		
+
 	// };
 
 	// P.curry = function() {
 
 	// }
 
-	P.mapObj = function(obj, cb) {
+	P.mapObj = function (obj, cb) {
 		var newObj = utils.splitObj(obj);
-		return Promise.map(newObj.values, cb)
-		.then(function(arr) {
+		return Promise.map(newObj.values, cb).then(function (arr) {
 			return utils.mergeObj(newObj.keys, arr);
-		})
-	}
+		});
+	};
 
-
-	P.syncAll = function(arr) {
+	P.syncAll = function (arr) {
 		var internalArr = [];
 		var chainedPromise = P.resolve(arr[0]);
-		for(var i = 1; i < arr.length; i++) {
-			chainedPromise.then(element => {internalArr.push(element)}).then(() => P.resolve(arr[i]))
-		}
-		return (
-			chainedPromise
-			.then(element => {
+		for (var i = 1; i < arr.length; i++) {
+			chainedPromise.then(function (element) {
 				internalArr.push(element);
-				return internalArr;
-			})
-		)
+			}).then(function () {
+				return P.resolve(arr[i]);
+			});
+		}
+		return chainedPromise.then(function (element) {
+			internalArr.push(element);
+			return internalArr;
+		});
 	};
 
 	// P.latch = function()
-
-
-	// P.sync = function(obj, cb) {
-
-	// }
-	//synchronous promises essentially	
-}
+};
 
--- utilities/index.js
+++ utilities/index.js
@@ -1,47 +1,45 @@
-'use strict'
+'use strict';
 
-var valueAccum = (value, array) => {
+var valueAccum = function valueAccum(value, array) {
 	array.push(value);
 	return array;
 };
 
-var splitObj = (obj) => {
+var splitObj = function splitObj(obj) {
 	var keys = Object.keys(obj);
-	var values = keys.map(function(key) {
+	var values = keys.map(function (key) {
 		return obj[key];
 	});
-	return {keys: keys, values: values};
-}
+	return { keys: keys, values: values };
+};
 
-var mergeObj = function(obj) {
-	var keys; var values;
-	if(arguments.length === 1) {
+var mergeObj = function mergeObj(obj) {
+	var keys;var values;
+	if (arguments.length === 1) {
 		keys = obj.keys;
 		values = obj.values;
 	} else {
 		keys = arguments[0];
 		values = arguments[1];
 	}
-	return keys.reduce(function(previous, current, index) {
+	return keys.reduce(function (previous, current, index) {
 		previous[current] = values[index];
 		return previous;
 	}, {});
-}
+};
 
 //recurisve version
-var flattenArr = function(arr) {
+var flattenArr = function flattenArr(arr) {
 	var newArr = [];
-	for(var i = 0; i < arr.length; i++) {
-		if(Array.isArray(arr[i])) newArr = newArr.concat(flattenArr(arr[i]));	
-		else newArr.push(arr[i]);
+	for (var i = 0; i < arr.length; i++) {
+		if (Array.isArray(arr[i])) newArr = newArr.concat(flattenArr(arr[i]));else newArr.push(arr[i]);
 	}
 	return newArr;
-}
-
-
+};
 
 module.exports = {
 	valueAccum: valueAccum,
 	splitObj: splitObj,
 	mergeObj: mergeObj
-}
+};
+
